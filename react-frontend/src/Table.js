d909b16a3892a6d2c8aec9d5f1c14ee94aa64798
nickambrose7 <nickambrose7@gmail.com>
2023-04-13 11:04:08 -0700

    Added table ID, done with asignment

--- react-frontend/src/Table.js
+++ react-frontend/src/Table.js
@@ -4,6 +4,7 @@ function TableHeader()  {
     return (
       <thead>
         <tr>
+          <th>ID</th>
           <th>Name</th>
           <th>Job</th>
           <th>Remove</th>
@@ -17,6 +18,7 @@ function TableHeader()  {
     const rows = props.characterData.map((row, index) => {
       return (
       <tr key={index}>
+        <td>{row.id}</td>
         <td>{row.name}</td>
         <td>{row.job}</td>
         <td>
